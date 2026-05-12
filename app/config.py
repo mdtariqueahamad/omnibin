165e47483b4653604591fb009e93fd2bf8fbe10b
mdtariqueahamad <mdtariqbist@gmail.com>
2026-05-12 23:25:44 +0530

    added login

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    mongodb_uri: str = "mongodb+srv://mdtariqbist_db_user:MongoAtlas%40786@cluster.chluorm.mongodb.net/?appName=Cluster"
+    mongodb_uri: str = "mongodb://localhost:27017"
     mongodb_db_name: str = "omnibin"
     mqtt_broker_host: str = "localhost"
     mqtt_broker_port: int = 1883
